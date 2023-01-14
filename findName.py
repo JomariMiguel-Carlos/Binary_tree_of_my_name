e1f0303e5590e0e0664590876cd6eb51fb1cbc58
JomariMiguel-Carlos <jomarimadla0@gmail.com>
2023-01-14 17:35:27 +0800

    Done with finding a value on the list and also the traversal method or order

---
+++ findName.py
@@ -0,0 +1,69 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+    def in_order_traversal(self):
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+        return elements
+
+
+def build_tree(elements):
+    print("This is my full name:",elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    Myname = ("J","O","M","A","R","I","M","I","G","U","E","L",
+              "M","C","A","R","L","O","S")
+    myname_tree = build_tree(Myname)
+
+    print("J is in the list? ", myname_tree.search("J"))
+    print("Z is in the list? ", myname_tree.search("Z"))
+    print("In order traversal gives this sorted list:", myname_tree.in_order_traversal())
+
